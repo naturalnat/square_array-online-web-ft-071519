7caaa3b72ccb4531adcbb5f1f6c6240fb363a31c
Natalie Taktachev <ntaktach@gmail.com>
2019-07-17 17:57:01 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,8 @@
 def square_array(numbers)
-  counter = 0
   new_numbers = []
-  while counter < numbers.length do
-    new_numbers.push(numbers[counter] ** 2)
-    counter += 1
+  array.each {
+    |element| new_numbers.push element ** 2
+  }
   end
   new_numbers
 end
